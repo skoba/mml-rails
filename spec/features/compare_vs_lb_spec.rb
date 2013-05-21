b162c2dd52de0bf5b3fdadaed8947b177a8eeaef
Shinji KOBAYASHI <skoba@moss.gr.jp>
2013-05-21 23:36:20 +0900

    TravisCI check

--- spec/features/compare_vs_lb_spec.rb
+++ spec/features/compare_vs_lb_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe "mmlvs/compare_vs_lb" do
-  before do
-    visit 'mmlvs/compare_vs_lb'
-  end
-
   it 'shows table with comparison of mmlVs and mmlLb' do
+    visit 'mmlvs/compare_vs_lb'
     page.should have_content 'mmlVs'
   end
 end
