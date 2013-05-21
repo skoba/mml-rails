bb78aeda43df944ee6036070f506264380d4c23a
Shinji KOBAYASHI <skoba@moss.gr.jp>
2013-05-21 23:37:01 +0900

    added /

--- spec/features/compare_vs_lb_spec.rb
+++ spec/features/compare_vs_lb_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "mmlvs/compare_vs_lb" do
   it 'shows table with comparison of mmlVs and mmlLb' do
-    visit 'mmlvs/compare_vs_lb'
+    visit '/mmlvs/compare_vs_lb'
     page.should have_content 'mmlVs'
   end
 end
