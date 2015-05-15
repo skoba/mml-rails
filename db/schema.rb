bd42bd07e9200cb531a7f60b49877db9d1767be3
Shinji KOBAYASHI <skoba@moss.gr.jp>
2015-05-15 10:11:54 +0900

    modifined database

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150513162430) do
   end
 
   create_table "injections", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
